11677a6a76126abc9289a6d829057b4ac3e1ec6f
David <Dcarlon00@yahoo.com>
2022-11-14 01:23:55 -0600

    pages and css updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,14 +4,14 @@ import 'react-toastify/dist/ReactToastify.css'
 import Dashboard from './pages/Dashboard'
 import Login from './pages/Login'
 import Register from './pages/Register'
-
+import Header from './components/Header'
 
 function App() {
   return (
     <>
     <Router>
         <div className='container'>
-        {/* <Header /> */}
+        <Header />
         <Routes>
           <Route path ='/' element = {<Dashboard />} />
           <Route path ='/Login' element = {<Login />} />
