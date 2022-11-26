fafc3180d8d94a3cec0f40a9e266509e7ea49f8b
David <Dcarlon00@yahoo.com>
2022-11-26 00:37:39 -0600

    added frontend files

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,8 +1,52 @@
 import React from 'react'
 
+import { Link } from "react-router-dom";
+import { Container, Row, Col, ListGroup, ListGroupItem } from "reactstrap";
+
+import heroImg from '../assets/images/restaruant2.png';
+import '../styles/hero-section.css';
+import Category from "../components/UI/category/Category.jsx";
+
+import "../styles/home.css";
+
+import featureImg01 from "../assets/images/service-01.png";
+import featureImg02 from "../assets/images/service-02.png";
+import featureImg03 from "../assets/images/service-03.png";
+
+import products from "../assets/fake-data/products.js";
+
+import foodCategoryImg01 from "../assets/images/hamburger.png";
+import foodCategoryImg02 from "../assets/images/pizza.png";
+import foodCategoryImg03 from "../assets/images/bread.png";
+
+import ProductCard from "../components/UI/product-card/ProductCard.jsx";
+
+import whyImg from "../assets/images/location.png";
+
+import networkImg from "../assets/images/network.png";
+
+import TestimonialSlider from "../components/UI/slider/TestimonialSlider.jsx";
+
 function Dashboard() {
   return (
-    <div>Current Page: Dashboard</div>
+    <section>
+      <Container>
+        <Row>
+          <Col lg='6' md='6'>
+            <div className="hero__content">
+
+            </div>
+          </Col>
+          <Col lg='6' md='6'>
+            <div className="hero__img">
+              <img src={heroImg} alt="hero-img" className="w-100" />
+            </div>
+          </Col>
+
+
+        </Row>
+      </Container>
+    </section>
   )
 }
 
